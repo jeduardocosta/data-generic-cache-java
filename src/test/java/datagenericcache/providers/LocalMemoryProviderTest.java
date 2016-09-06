54e3b21e701d203b5887a16e1d6257a43098bf2b
jeduardocosta <eduardocastro.jj@gmail.com>
2016-09-06 12:58:45 -0300

    create 'cacheProviderIntegrationTest' base class and used in all available providers

--- src/test/java/datagenericcache/providers/LocalMemoryProviderTest.java
+++ src/test/java/datagenericcache/providers/LocalMemoryProviderTest.java
@@ -1,66 +1,10 @@
 package datagenericcache.providers;
 
-import java.time.Duration;
+import org.junit.Before;
 
-import org.junit.*;
-
-import static org.awaitility.Awaitility.*;
-import static java.util.concurrent.TimeUnit.*;
-import static org.junit.Assert.*;
-
-public class LocalMemoryProviderTest {
-    private CacheProvider cacheProvider;
-
-    private final String key = "key";
-    private final String value = "value";
-
+public class LocalMemoryProviderTest extends CacheProviderIntegrationTest {
     @Before
     public void before() {
-        cacheProvider = new LocalMemoryProvider();
-    }
-
-    @Test
-    public void shouldRetrieveNullWhenValueDoesntExistInLocalMemoryCacher() {
-        String obtained = cacheProvider.retrieve("whenValueDoesntExistKey");
-
-        assertNull(obtained);
-    }
-
-    @Test
-    public void shouldAddAndRetrieveSuccessfullInLocalMemoryCacher() {
-        cacheProvider.add(key, value, Duration.ofSeconds(1));
-
-        assertEquals(value, cacheProvider.retrieve(key));
-    }
-
-    @Test
-    public void shouldAddWhenInvokingRetrieveOrElseInLocalMemoryCacher() {
-        cacheProvider.retrieveOrElse(key, Duration.ofSeconds(1), () -> value);
-
-        assertEquals(value, cacheProvider.retrieve(key));
-    }
-
-    @Test
-    public void shouldExpireWhenAddWithTimeSpanInLocalMemoryCacher() {
-        cacheProvider.add(key, value, Duration.ofMillis(50));
-
-        await()
-                .atMost(110, MILLISECONDS)
-                .until(() -> cacheProvider.retrieve(key) == null);
-    }
-
-    @Test
-    public void shouldCheckIfValueExistsInLocalMemoryCacher() {
-        cacheProvider.add(key, value, Duration.ofSeconds(1));
-
-        assertTrue(cacheProvider.exists(key));
-    }
-
-    @Test
-    public void shouldRemoveValueInLocalMemoryCacher() {
-        cacheProvider.add(key, value, Duration.ofSeconds(1));
-        cacheProvider.remove(key);
-
-        assertFalse(cacheProvider.exists(key));
+        super.cacheProvider = new LocalMemoryProvider();
     }
 }
--- src/test/java/datagenericcache/providers/RedisProviderIntegrationTest.java
+++ src/test/java/datagenericcache/providers/RedisProviderIntegrationTest.java
@@ -1,79 +1,17 @@
 package datagenericcache.providers;
 
-import org.junit.*;
+import org.junit.Before;
+import org.junit.ClassRule;
 import org.testcontainers.containers.GenericContainer;
 
-import java.time.Duration;
-
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static org.awaitility.Awaitility.await;
-import static org.junit.Assert.*;
-
-public class RedisProviderIntegrationTest {
+public class RedisProviderIntegrationTest extends CacheProviderIntegrationTest {
     @ClassRule
     public static GenericContainer redisContainer= new GenericContainer("redis:3.0.7").withExposedPorts(6379);
 
-    private CacheProvider cacheProvider;
-
-    private final String key = "key";
-    private final String value = "value";
-
     @Before
     public void before() {
-        if (redisContainer.isRunning()) {
-            String host = redisContainer.getContainerIpAddress();
-            int portNumber = redisContainer.getMappedPort(6379);
-            cacheProvider = new RedisProvider(host, portNumber);
-        }
-    }
-
-    @After
-    public void after() {
-        cacheProvider.flush();
-    }
-
-    @Test
-    public void shouldRetrieveNullWhenValueDoesntExistInRedisLiveProvider() {
-        String obtained = cacheProvider.retrieve("whenValueDoesntExistKey");
-
-        assertNull(obtained);
-    }
-
-    @Test
-    public void shouldAddAndRetrieveSuccessfullInRedisLiveProvider() {
-        cacheProvider.add(key, value, Duration.ofSeconds(1));
-
-        assertEquals(value, cacheProvider.retrieve(key));
-    }
-
-    @Test
-    public void shouldAddWhenInvokingRetrieveOrElseInRedisLiveProvider() {
-        cacheProvider.retrieveOrElse(key, Duration.ofSeconds(1), () -> value);
-
-        assertEquals(value, cacheProvider.retrieve(key));
-    }
-
-    @Test
-    public void shouldExpireWhenAddWithTimeSpanInRedisLiveProvider() {
-        cacheProvider.add(key, value, Duration.ofMillis(50));
-
-        await()
-                .atMost(110, MILLISECONDS)
-                .until(() -> cacheProvider.retrieve(key) == null);
-    }
-
-    @Test
-    public void shouldCheckIfValueExistsInRedisLiveProvider() {
-        cacheProvider.add(key, value, Duration.ofSeconds(1));
-
-        assertTrue(cacheProvider.exists(key));
-    }
-
-    @Test
-    public void shouldRemoveValueInRedisLiveProvider() {
-        cacheProvider.add(key, value, Duration.ofSeconds(1));
-        cacheProvider.remove(key);
-
-        assertFalse(cacheProvider.exists(key));
+        String host = redisContainer.getContainerIpAddress();
+        int portNumber = redisContainer.getMappedPort(6379);
+        super.cacheProvider = new RedisProvider(host, portNumber);
     }
 }
